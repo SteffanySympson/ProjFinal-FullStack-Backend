9e4739f10ed9722f7a40fb5643708110d5a52cc4
Steffany Sympson <ssy.marques@gmail.com>
2021-10-19 16:48:12 -0300

    ProjFinal ToDo Brack

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-if(process.env.NODE_ENV !== 'production'){
+if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
 const express = require('express');
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+}
+
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 
-const Conn = require('./models/conn/conn');
+const Connection = require('./connection/connection');
 
 const db_url = process.env.DB_URL;
 const db_user = process.env.DB_USER;
 const db_pass = process.env.DB_PASS;
 const db_data = process.env.DB_DATA;
 
-Conn(db_url, db_user, db_pass, db_data);
+Connection(db_url, db_user, db_pass, db_data);
 
-const port = 3000;
+const rotasTodolist = require('./routers/app.rotas.js');
+app.use('/todolist', rotasTodolist);
 
-const tarefa = require('./routers/todolist.routes')
-app.use('/tarefas', tarefa);
-
-app.listen(port, () => {
-    console.info(`Servidor rodando na porta ${port}`)
+const port = 4000;
+app.listen(process.env.PORT || port, () => {
+    console.log(`O servidor da aplicação está rodando em: http://localhost:${port}`);
 });
